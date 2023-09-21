4018695f8b0d06720edd8b6e868c1039339fc440
Priːst ☦ <127801801+CelestialScripter@users.noreply.github.com>
2023-09-21 10:34:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as file:
+    requirements = file.read().splitlines()
+
+setup(
+    name="save_skype",
+    version="0.1.5",
+    description="Extract and save Skype chats.",
+    url="https://github.com/thismachinechills/save_skype",
+    author="thismachinechills (Alex)",
+    license="AGPL 3.0",
+    packages=['save_skype'],
+    zip_safe=True,
+    install_requires=requirements,
+    keywords="skype main.db extract chats".split(),
+    entry_points={
+        "console_scripts": [
+            "save_skype = save_skype.extract:chats_to_files"
+        ]
+    }
+)
